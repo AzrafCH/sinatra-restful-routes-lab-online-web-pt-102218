f26598696324e881955e5a216fcec162965b792d
Azraf Chowdhury <achowdhur@ltu.edu>
2019-02-17 23:15:51 +0000

    Automatically backed up by Learn

--- db/migrate/20190217224931_create_recipes.rb
+++ db/migrate/20190217224931_create_recipes.rb
@@ -2,9 +2,9 @@ class CreateRecipes < ActiveRecord::Migration
 
   def change
     create_table :recipes do |t|
-      t.string :name 
+      t.string :name
       t.string :ingredients
-      t.string :cook_time 
-    end 
+      t.string :cook_time
+    end
   end
 end
